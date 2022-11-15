2d6dc2a71ee67083d9655543097fdb04e73d7cc6
zhangsan <lxs2048@sina.com>
2022-11-16 00:01:01 +0800

    session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  app.use(
+    session({
+      secret: 'zhangsan',
+      name: 'zhangsan.sid',
+      rolling: true,
+      cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,17 +12,52 @@ import {
   Headers,
   HttpCode,
   Redirect,
+  Req,
+  Res,
+  Session,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import * as svgCaptcha from 'svg-captcha';
 @Controller({
   path: 'user',
   version: '1', // 升级版本
 })
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('code')
+  createCaptcha(@Req() req, @Res() res) {
+    const captcha = svgCaptcha.create({
+      size: 4, //生成几个验证码
+      fontSize: 50, //文字大小
+      width: 100, //宽度
+      height: 34, //高度
+      background: '#cc9966', //背景颜色
+    });
+    req.session.code = captcha.text; //存储验证码记录到session
+    console.log(req.session.code);
+    res.type('image/svg+xml');
+    res.send(captcha.data);
+  }
+  @Post('create')
+  createUser(@Session() session, @Body() body) {
+    console.log(session, body);
+    if (
+      session.code &&
+      session.code.toLocaleLowerCase() === body?.code?.toLocaleLowerCase()
+    ) {
+      return {
+        message: '验证码正确',
+      };
+    } else {
+      return {
+        message: '验证码错误',
+      };
+    }
+  }
 
   @Post()
   create(@Body() body) {
@@ -35,7 +70,6 @@ export class UserController {
 
   @Get()
   @HttpCode(500)
-  @Redirect('/34')
   findAll(@Query() query, @Headers() headers) {
     console.log(headers);
     return {
