a4cc3188d1e6ccfb63f5a8890e725a38ee007215
zhangsan <lxs2048@sina.com>
2022-11-27 18:59:09 +0800

    连接mysql

--- src/list/entities/list.entity.ts
+++ src/list/entities/list.entity.ts
@@ -1,1 +1,11 @@
-export class List {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class List {
+  //自增列
+  @PrimaryGeneratedColumn()
+  id: number;
+  //普通列
+  @Column()
+  title: string;
+}
